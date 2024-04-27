00219e2456cbb927ba541b6ba209b6ff8bba7062
thedistortedwajdan <wajdan.mohammad@gmail.com>
2024-04-28 02:53:54 +0500

    VERSION 1.0 had to get a secure api xddd

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -3,11 +3,22 @@ import axios from "axios";
 import { useEffect } from "react";
 
 export default function Text(props) {
+  const options = {
+    method: "GET",
+    url: "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
+    headers: {
+      accept: "application/json",
+      "X-RapidAPI-Key": "e53997a212msh548bd9c442d8daep1ce11ajsn07ee38427df0",
+      "X-RapidAPI-Host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+    },
+  };
   useEffect(() => {
     const getText = async () => {
       try {
-        const data = await axios.get("http://metaphorpsum.com/sentences/3");
-        props.settext(data.data);
+        const response1 = await axios.request(options);
+        const response2 = await axios.request(options);
+        const response = response1.data.value + " " + response2.data.value;
+        props.settext(response);
       } catch (error) {
         alert("Text.js ", error.message);
       }
